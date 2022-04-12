6148d07d53058c3b840d3c7d7e1304ef1232add7
danielhan0708 <69658491+danielhan0708@users.noreply.github.com>
2022-04-12 09:59:06 +0900

    커밋

---
+++ practice.py
@@ -0,0 +1,38 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+# print("ㅋ"*9)
+# 참 / 거짓
+# print(5>10)
+# print(5<10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not(5>10))
+## 애완동물을 소개해 주세요~
+# animal ="강아지"
+# name ="연탄이"
+# age = 4
+# hobby = "산책"
+# is_adult = age >= 3
+# print("우리집 "+ animal +"의 이름은 "+ name +"예요")
+# ## print(name +"는 "+ str(age) +"살이며, "+ hobby +"을 아주 좋아해요")
+# print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+# print(name +"는 어른일까요?"+ str(is_adult))
+## Quiz) 변수를 이용하여 다음문장을 출력하시오
+## 변수명 : station
+## 변수값 : "사당", "신도림", "인천공항" 순서대로 입력
+## 출력문장 : XX 행 열차가 들어고 있습니다.
+# station = ["사당", "신도림", "인천공항"]
+# print(station[0], "행 열차가 들어오고 있습니다.")
+# print(station[1], "행 열차가 들어오고 있습니다.")
+# print(station[2], "행 열차가 들어오고 있습니다.")
+print(1+1)
