bbc8c95cdfa8fc8b663ef0ca57c3b34a789d04b8
danielhan0708 <69658491+danielhan0708@users.noreply.github.com>
2022-07-04 10:35:32 +0900

    commit

--- practice.py
+++ practice.py
@@ -523,4 +523,137 @@
 # profile(main_lang="자바", age=25, name="김태호")
 
 ##가변 인자
- 
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     print(lang1, lang2, lang3, lang4, lang5)
+# def profile(name, age, *langauge):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     for lang in langauge:
+#         print(lang, end=" ")
+#     print()
+# profile("유재석", 20, "Python", "Java", "C", "C++", "C#", "JavaScript")
+# profile("김태호", 25, "Kotlin", "Swift")
+
+##지역변수와 전역변수
+gun = 10
+# def checkpoint(soldiers): #경계근무
+#     global gun #전역 공간에 있는 gun을 사용
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+# print("전체 총 : {0}".format(gun))
+# checkpoint(2) #2명이 경계 근무
+# print("남은 총 : {0}".format(gun))
+# def checkpoint_ret(gun, soldiers): #경계근무
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+#     return gun
+# print("전체 총 : {0}".format(gun))
+# gun = checkpoint_ret(gun, 2) #2명이 경계 근무
+# print("남은 총 : {0}".format(gun))
+
+# ##퀴즈 6
+# Quiz) 표준 체중을 구하는 프로그램을 작성하시오
+# * 표준 체중 : 각 개인의 키에 적당한 체중
+# (성별에 따른 공식)
+# 남자 : 키(m) X 키(m) X 22
+# 여자 : 키(m) X 키(m) X 21
+# 조건1 : 표준 체중은 별도의 함수 내에서 계산
+#         * 함수명 : std_weight
+#         * 전달값 : 키(height), 성별(gender)
+# 조건2 : 표준 체중은 소수점 둘째자리까지 표시
+# (출력 예제)
+# 키 175cm 남자의 표준 체중은 67.38kg 입니다.
+# #풀이
+# height =  175
+# gender = '남자'
+# def std_weight(height, gender):
+#     if gender == '남자' :
+#         return height * height * 22
+#     elif gender == '여자' :
+#         return height * height * 21
+#     else :
+#         print("잘못된 입력입니다.")
+# weight = round(std_weight(height/100, gender),2)
+# print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
+
+##표준 입출력 
+# print("Python", "Java", sep=",", end="?")
+# print("무엇이 더 재밌을까요?")
+# import sys
+# print("Pyton", "Java", file=sys.stdout) #표준 출력
+# print("Pyton", "Java", file=sys.stderr) #표준 에러처리
+# scores = {"수학" : 0, "영어" : 50, "코딩" : 100}
+# for subject, score in scores.items():
+#     # print(suject, score)
+#     print(subject.ljust(8), str(score).rjust(4), sep=":")
+# 은행 대기순번표
+# for num in range(1, 21):
+#     print("대기번호 : " + str(num).zfill(3))
+# answer = input("아무 값이나 입력하세요 : ")
+# print("입력하신 값은 " + answer + "입니다.")
+
+## 다양한 출력 포맷
+# # 빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+# print("{0: >10}".format(500))
+# # 양수일 땐 +로 표시, 음수일 땐 -로 표시
+# print("{0: >+10}".format(500))
+# print("{0: >+10}".format(-500))
+# # 왼쪽 정렬하고, 빈칸으로 _로 채움
+# print("{0:_<10}".format(500))
+# # 3자리 마다 콤마를 찍어주기
+# print("{0:,}".format(100000000000000))
+# # 3자리 마다 콤마를 찍어주기,  +- 부호도 붙이기
+# print("{0:+,}".format(100000000000000))
+# print("{0:+,}".format(-100000000000000))
+# # 3자리 마다 콤마를 찍어주기, 부호도 붙이고, 자릿수 확보하기
+# # 돈이 많으면 행복하니까 빈 자리는 ^ 로 채우기
+# print("{0:^<+30,}".format(100000000000000))
+# # 소수점 출력
+# print("{0:f}".format(5/3))
+# # 소수점 특정 자리수 까지만 표시 (소수점 3째 자리에서 반올림)
+# print("{0:.2f}".format(5/3))
+
+## 파일 입출력
+# score_file = open("score.txt", "w", encoding="utf8") #파일에 쓰기
+# print("수학 : 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
+# score_file = open("score.txt", "a", encoding="utf8") #파일에 이어쓰기
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+# score_file = open("score.txt", "r", encoding="utf8") #파일 읽어오기
+# print(score_file.read()) #전체읽기
+# score_file.close()
+# score_file = open("score.txt", "r", encoding="utf8") #파일 읽어오기
+# print(score_file.readline(), end="") #줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# score_file.close()
+# score_file = open("score.txt", "r", encoding="utf8") #파일 읽어오기
+# while True:                                         #파일의 줄 갯수를 모를 때 반복문으로 처리
+#     line =  score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+# score_file = open("score.txt", "r", encoding="utf8") #파일 읽어오기
+# lines = score_file.readlines() # list 형태로 저장
+# for line in lines:
+#     print(line, end="")
+# score_file. close()
+
+##pickle
+# import pickle
+# profile_file = open("profile.pickle", "wb") #피클 바이너리 형태로 해야 함
+# profile = {"이름":"박명수", "나이":30, "취미": ["축구", "골프", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) #profile에 있는 정보를 file에 저장
+# profile_file.close()
+# profile_file = open("profile.pickle", "rb") #피클 바이너리 형태로 해야 함
+# profile = pickle.load(profile_file) #file에 있는 정보를 profile에 불러오기
+# print(profile)
+# profile_file.close()
+
+##with 02:31:04
