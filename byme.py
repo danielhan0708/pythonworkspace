bb56273eb366748e3ff32f01c796f3f4b6bb0b57
danielhan0708 <69658491+danielhan0708@users.noreply.github.com>
2022-08-27 16:11:07 +0900

    finish_class

---
+++ byme.py
@@ -0,0 +1,4 @@
+def sign():
+    print("이 프로그램은 다니엘에 의해 만들어졌습니다.")
+    print("유튜브 : http://youtube.com")
+    print("이메일 : d.s.han0708@gmail.com")
--- practice.py
+++ practice.py
@@ -535,7 +535,7 @@
 # profile("김태호", 25, "Kotlin", "Swift")
 
 ##지역변수와 전역변수
-gun = 10
+# gun = 10
 # def checkpoint(soldiers): #경계근무
 #     global gun #전역 공간에 있는 gun을 사용
 #     gun = gun - soldiers
@@ -656,4 +656,378 @@ gun = 10
 # print(profile)
 # profile_file.close()
 
-##with 02:31:04
+##with
+# import pickle
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+# with open("study.txt", "w", encoding="utf-8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+# with open("study.txt", "r", encoding="utf-8") as study_file:
+#     print(study_file.read())
+
+# ## 퀴즈7
+# Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+# 보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
+# - X 주차 주간 보고 -
+# 부서 :
+# 이름 :
+# 업무요약 :
+# 1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
+# 조건 :  파일명은  '1주차.txt', '2주차.txt', ... 와 같이 만듭니다.
+## 3:33:35
+# for week in range(1, 51):
+#     with open("{0}주차.txt".format(week), "w",encoding="utf-8") as report_file:
+#         report_file.write(" - {0} 주차 주간 보고 -".format(week))
+#         report_file.write("\n부서 : ")
+#         report_file.write("\n이름 : ")
+#         report_file.write("\n업무요약 : ")
+
+# ## Class , __init__, 멤버변수
+# # 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
+# name = "마린" # 유닛의 이름
+# hp = 40       # 유닛의 체력
+# damage = 5    # 유닛의 공격력
+# print("{} 유닛이 생성 되었습니다.".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+# # 탱크 : 공격 유닛, 탱크. 포을 쏠 수 있음, 일반모드 / 시즈모드
+# tank_name = "탱크" # 유닛의 이름
+# tank_hp = 150       # 유닛의 체력
+# tank_damage = 35    # 유닛의 공격력
+# print("{} 유닛이 생성 되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(name,location,damage))
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+# class Unit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+#레이스 : 공중 유닛, 비행기, 클로킹(상대방에게 보이지 않음)
+# wraith1 = Unit("레이스", 80, 5)
+# print("유닛 이름 :  {0}, 공격력 : {1}".format(wraith1.name,wraith1.damage))
+
+## 메소드, 상속
+# class Unit:
+#     def __init__(self, name, hp, speed):
+#         self.name = name
+#         self.hp = hp
+#         self.speed = speed
+#     def move(self, location):
+#         print("[지상 유닛 이동]")
+#         print("{0} : {1} 방향으로 이동합니다. [속도{2}]".format(self.name, location, self.speed))
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, speed, damage):
+#         Unit.__init__(self, name, hp, speed)
+#         self.damage = damage
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(self.name, location, self.damage))
+#     def damaged(self, damage):
+#         print("{0} : {1}데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은  {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴 되었습니다.".format(self.name))
+# #파이어뱃 : 공격 유닛, 화염 방사기.
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+# # 공격 2번 받는다고 가정
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+
+## 다중상속 (상속에 이어서)
+# 드라쉽 : 공중 유닛, 수송기. 공격X
+# 날 수 있는 기능을 가진 클래스
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_seed = flying_speed
+#     def fly(self, name, location):
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_seed))
+# # 공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, 0, damage) #지상 유닛의 speed 0으로 처리
+#         Flyable.__init__(self, flying_speed)
+#     def move(self, location):
+#         print("[공중 유닛 이동]")
+#         self.fly(self.name, location)
+# 발키리 : 공중공격 유닛, 한번에 14발 미사일 발사
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시") 
+
+## 연산자 오버로딩        
+# # 벌쳐 : 지상 유닛, 기동성이 좋음
+# vulture = AttackUnit("벌쳐", 80, 10, 20)
+# # 배틀크루져 : 공중 유닛, 체력도 굉장히 좋음, 공격력도 좋음.
+# battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+# vulture.move("11시")
+# # battlecruiser.fly(battlecruiser.name, "9시")
+# battlecruiser.move("9시")
+
+## Pass
+# 건물
+# class BuildingUnit(Unit):
+#     def __init__(self, name, hp, location):
+#         #pass
+#         # Unit.__init__(self, name, hp, 0)
+#         super().__init__(name, hp, 0)
+#         self.location = location
+# #서플라이 디폿 : 건물, 1개 건물 = 8 유닛 생산.
+# supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
+# def game_start():
+#     print("[알림] 새로운 게임을 시작합니다.")
+# def game_over():
+#     pass     
+# game_start()
+# game_over()
+
+##super
+# class Unit_1:
+#     def __init__(self):
+#         print("Uint 생성자")
+# class Flyable_1:
+#     def __init__(self):
+#         print("Flyable 생성자")
+# class FlyableUnit_1(Unit_1, Flyable_1):
+#     def __init__(self):
+#         # super().__init__()
+#         Unit_1.__init__(self)
+#         Flyable_1.__init__(self)
+# #드라쉽
+# dropship = FlyableUnit_1()
+
+## 퀴즈 8
+# #Quiz) 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.
+# #(출력예제)
+# #총 3대의 매물이 있습니다.
+# #강남 아파트 매매 10억 2010년
+# #마포 오피스텔 전세 5억 2007년
+# #송파 빌라 월세 500/50 2000년
+# #[코드]
+# class House:
+#     #매물 초기화
+#     def __init__(self, locaion, house_type, deal_type, price, completion_year):
+#         self.location = locaion
+#         self.house_type = house_type
+#         self.deal_type = deal_type
+#         self.price = price
+#         self.completion_year = completion_year
+#     #매물 정보 표시
+#     def show_detail(self):
+#         print("{0} {1} {2} {3} {4}".format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
+# h1 = House("강남","아파트","매매","10억","2010년")
+# h2 = House("마포","오피스텔","전세","5억","2007년")
+# h3 = House("송파","빌라","월세","500/50","2000년")
+# houses =[]
+# houses.append(h1)
+# houses.append(h2)
+# houses.append(h3)
+# print("총 {0}대의 매물이 있습니다.".format(len(houses)))
+# for unit in houses:
+#     unit.show_detail()
+    
+ ## 예외처리   
+# try: 
+#     print("나누기 전용 계산기 입니다.")
+#     # num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     # num2 = int(input("두 번째 숫자를 입력하세요 : "))    
+#     # print("{0} / {1} = {2}".format(num1, num2, int(num1/num2)))
+#     nums =[]
+#     nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
+#     nums.append(int(input("두 번째 숫자를 입력하세요 : ")))    
+#     # nums.append(int(nums[0]/nums[1]))
+#     print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
+# except ValueError:
+#     print("에러! 잘못된 값을 입력하였습니다.")
+# except ZeroDivisionError as err:
+#     print(err)
+# except Exception as err:
+#     print("알수 없는 에러가 잘생하였습니다.")
+#     print(err)
+
+## 에러 만들기
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("두 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise ValueError
+#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+
+## 사용자 정의 예외처리 , finally
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+#     def __str__(self):
+#         return self.msg
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("두 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+# except BigNumberError as err:
+#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요")
+#     print(err)
+# finally:
+#     print("계산기를 이용해 주셔서 감사합니다.")
+
+## 퀴즈 9
+# Quiz) 동네에 항상 대기 손님이 있는 맛있는 치킨집이 있습니다.
+# 대기 손님의 치킨 요리 시간을 줄이고자 디자동 주문 시스템을 제작하였습니다.
+# 시스템 코드를 확인하고 적절한 예외처리 구문을 넣으시오.
+# 조건1 : 1보다 작거나 숫자가 아닌 입력값이 들어올 때는 ValueError 로 처리
+#         출력 메세지 : "잘못된 값을 입력하였습니다."
+# 조건2 : 대기 손님이 주문할 수 있는 총 치킨량은 10마리로 한정
+#         치킨 소진 시 사용자 정의 에러[SoldOutError]를 발생 시키고 프로그램 종료
+#         출력 메세지 : "재고가 소진되어 더 이상 주문을 받지 않습니다."
+# [코드]
+# chicken = 10
+# waiting = 1 # 홀 안에는 현재 만석. 대기번호 1부터 시작
+# class SoldOutError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+#     def __str__(self):
+#         return self.msg
+# while(True):
+#     try:
+#         print("[남은 치킨 : {0}]".format(chicken))
+#         order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+#         if order > chicken: # 남은 치킨보다 주문량이 많을 때
+#             print("재료가 부족합니다.")
+#         elif order < 1 :
+#             raise ValueError
+#         else:
+#             print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다.".format(waiting, order))
+#             waiting += 1
+#             chicken -= order
+#             if chicken == 0:
+#                 raise SoldOutError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#     except ValueError:
+#         print("잘못된 값을 입력하였습니다.")
+#     except SoldOutError as err:
+#         print(err)
+#         break
+
+## 모듈
+# import thrater_module
+# thrater_module.price(3) #3명이서 영화 보러 갔을 때 가격
+# thrater_module.price_morning(4) # 4명이서 조조 할인 영화 보러 갔을 때
+# thrater_module.price_soldier(5) # 5명의 군인이 영화 보러 갔을 때
+# import thrater_module as mv
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+# from thrater_module import *
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+# from thrater_module import price, price_morning
+# price(5)
+# price_morning(6)
+# from thrater_module import price_soldier as ps
+# ps(5)
+
+## 패키지
+# import travel.thailand
+# trip_to = travel.thailand.TailandPackage()
+# trip_to.detail()
+# from travel.thailand import TailandPackage
+# trip_to = TailandPackage()
+# trip_to.detail()
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+## __all__ , 모듈 직접 실행
+# from travel import *
+# # trip_to = vietnam.VietnamPackage()
+# trip_to = thailand.TailandPackage()
+# trip_to.detail()
+
+##  패키지, 모듈 위치
+# import inspect
+# import random
+# print(inspect.getfile(random))
+# from travel import *
+# print(inspect.getfile(thailand))
+
+## pip install
+## https://pypi.org/project/beautifulsoup4/
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+# print(soup.prettify())
+
+## 내장함수
+# #input : 사용자 입력을 받는 함수
+# language = input("무슨 언어를 좋아하세요?")
+# print("{0}은 아주 좋은 언어입니다.".format(language))
+# # dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+# print(dir())
+# import random #외장함수
+# print(dir())
+# import pickle
+# print(dir())
+# print(dir(random))
+# lst = [1, 2, 3]
+# print(dir(lst))
+# name ="Jim"
+# print(dir(name))
+##https://docs.python.org/ko/3/library/functions.html
+
+## 외장함수
+##https://docs.python.org/ko/3/py-modindex.html
+# # glob : 경로 내에 폴더 / 파일 목록 조회 (윈도우 dir)
+# import glob
+# print(glob.glob("*.py"))
+# # os : 운영체제에서 제공하는 기본 기능
+# import os
+# print((os.getcwd())) # 현재 디렉토리
+# folder = "sample_dir"
+# if os.path.exists(folder):
+#     print("이미 존재하는 폴더입니다.")
+#     os,os.rmdir(folder)
+#     print(folder,"폴더를 삭제하였습니다.")
+# else:
+#     os.makedirs(folder) #폴더 생성
+#     print(folder, "폴더를 생성하였습니다.")
+# print((os.listdir))
+# # time
+# import time
+# print(time.localtime())
+# print(time.strftime("%y-%m-%d %H:%M:%S"))
+# import datetime
+# print("오늘날짜는 ", datetime.date.today())
+# # timedelta : 두 날짜 사이의 간격
+# today = datetime.date.today() # 오늘 날짜 저장
+# td = datetime.timedelta(days=100) # 100일 저장
+# print("우리가 만난지 100일은 ", today + td) # 오늘부터 100일 후
+
+## 퀴즈 10
+# Quiz) 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오
+# 조건 : 모듈 파일명은 byme.py 로 저장
+# (모듈 사용 예제)
+# import byme
+# byme.sign()
+# (출력 예제)
+# 이 프로그램은 다니엘에 의해 만들어졌습니다.
+# 유튜브 : http://youtube.com
+# 이메일 : d.s.han0708@gmail.com
+# with open("byme.py", "w",encoding="utf-8") as report_file:
+#         report_file.write("def sign():")
+#         report_file.write("\n    print(\"이 프로그램은 다니엘에 의해 만들어졌습니다.\")")
+#         report_file.write("\n    print(\"유튜브 : http://youtube.com\")")
+#         report_file.write("\n    print(\"이메일 : d.s.han0708@gmail.com\")")
+# import byme
+# byme.sign()
